8060af5da34520bd0da9a17eb30078d02099cd63
Fouad Roumieh <froumieh@truemark.io>
2024-04-04 13:43:34 +0200

    Add os domain config

--- cdk/lib/overwatch-construct.ts
+++ cdk/lib/overwatch-construct.ts
@@ -1,4 +1,8 @@
 import {Construct} from 'constructs';
+import {Domain, EngineVersion} from 'aws-cdk-lib/aws-opensearchservice';
+import {PolicyStatement, AnyPrincipal, Effect} from 'aws-cdk-lib/aws-iam';
+import {EbsDeviceVolumeType} from 'aws-cdk-lib/aws-ec2';
+import {RemovalPolicy} from 'aws-cdk-lib';
 
 export class OverwatchConstruct extends Construct {
   constructor(scope: Construct, id: string) {
@@ -18,5 +22,64 @@ export class OverwatchConstruct extends Construct {
       The Domain will be public
       Deploy into your dev account for now
     */
+
+    // Create OpenSearch Domain
+    const domain = new Domain(this, 'LogsOpenSearchDomain', {
+      version: EngineVersion.OPENSEARCH_2_11,
+      removalPolicy: RemovalPolicy.DESTROY,
+      domainName: 'os-logs-domain',
+      enableAutoSoftwareUpdate: true,
+      capacity: {
+        warmNodes: 3,
+        masterNodes: 3,
+        dataNodes: 3,
+        dataNodeInstanceType: 'r5.large.search',
+        masterNodeInstanceType: 'r5.large.search',
+        warmInstanceType: 'ultrawarm1.medium.search',
+      },
+      zoneAwareness: {
+        enabled: true,
+        availabilityZoneCount: 3,
+      },
+      ebs: {
+        volumeSize: 10, // GiB
+        volumeType: EbsDeviceVolumeType.GENERAL_PURPOSE_SSD_GP3,
+      },
+      logging: {
+        slowSearchLogEnabled: true,
+        appLogEnabled: true,
+        slowIndexLogEnabled: true,
+        auditLogEnabled: true,
+      },
+      encryptionAtRest: {
+        enabled: true,
+      },
+      nodeToNodeEncryption: true,
+      enforceHttps: true,
+      useUnsignedBasicAuth: false,
+      enableVersionUpgrade: true,
+      fineGrainedAccessControl: {
+        masterUserName: 'logsadmin',
+        // samlAuthenticationEnabled: true,
+        // samlAuthenticationOptions: {
+        //   idpEntityId: 'entity-id',
+        //   idpMetadataContent: 'metadata-content-with-quotes-escaped',
+        // },
+      },
+    });
+
+    domain.addAccessPolicies(
+      new PolicyStatement({
+        actions: [
+          'es:ESHttpPost',
+          'es:ESHttpPut',
+          'es:ESHttpGet',
+          'es:ESHttpDelete',
+        ],
+        effect: Effect.ALLOW,
+        principals: [new AnyPrincipal()],
+        resources: [domain.domainArn, `${domain.domainArn}/*`],
+      })
+    );
   }
 }
