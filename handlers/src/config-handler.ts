2568a9b347ce04fa61ab67b644373bf5ba16d804
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-05-21 15:54:57 -0600

    Refactoring code and adding OpenSearchClient

--- handlers/src/config-handler.ts
+++ handlers/src/config-handler.ts
@@ -1,5 +1,9 @@
 import * as logging from '@nr1e/logging';
-import {getOpenSearchClient} from './open-search-helper';
+import {
+  getOpenSearchClient,
+  OpenSearchClient,
+  PartialIsmPolicy,
+} from './open-search-helper';
 
 const log = logging.getLogger('config-handler');
 
@@ -9,142 +13,55 @@ export async function handler(): Promise<void> {
     svc: 'overwatch',
   });
   const client = await getOpenSearchClient();
-  const info = await client.info();
-  log.info().obj('info', info).msg('Retrieved OpenSearch info');
-  await createOrUpdateISMPolicy(log);
+  await createOrUpdateISMPolicy(client);
   return;
 }
-async function createOrUpdateISMPolicy(log: any) {
-  const policyName = 'delete_logs_after_90_days';
-  const policy = {
-    policy: {
-      description: 'Manage index lifecycle',
-      default_state: 'hot',
-      states: [
-        {
-          name: 'hot',
-          actions: [],
-          transitions: [
-            {
-              state_name: 'delete',
-              conditions: {
-                min_index_age: '1d',
-              },
+async function createOrUpdateISMPolicy(client: OpenSearchClient) {
+  const policyId = 'delete_logs_after_90_days';
+  const policy: PartialIsmPolicy = {
+    description: 'Manage index lifecycle',
+    default_state: 'hot',
+    states: [
+      {
+        name: 'hot',
+        actions: [],
+        transitions: [
+          {
+            state_name: 'delete',
+            conditions: {
+              min_index_age: '90d',
             },
-          ],
-        },
-        {
-          name: 'delete',
-          actions: [
-            {
-              delete: {},
-            },
-          ],
-          transitions: [],
-        },
-      ],
-      ism_template: [
-        {
-          index_patterns: ['logs-*'],
-          priority: 100,
-        },
-      ],
-    },
+          },
+        ],
+      },
+      {
+        name: 'delete',
+        actions: [
+          {
+            delete: {},
+          },
+        ],
+        transitions: [],
+      },
+    ],
+    ism_template: [
+      {
+        index_patterns: ['logs-*'],
+        priority: 100,
+      },
+    ],
   };
 
-  const policyPath = `/_plugins/_ism/policies/${policyName}`;
+  const policyVersion = await client.ism.getPolicy(policyId);
+  if (policyVersion === null) {
+    throw new Error(`Policy ${policyId} not found.`);
+  }
 
-  //Fetch the existing policy version
-  const policyVersion: any = await fetchPolicy(policyPath, log);
-
-  //Update the policy
-  await post(
-    policyPath,
+  await client.ism.updatePolicy(
+    policyId,
     policy,
-    policyVersion?.seq_no,
-    policyVersion?.primary_term,
-    log
+    policyVersion._seq_no,
+    policyVersion._primary_term
   );
-}
-async function fetchPolicy(
-  policyPath: string,
-  log: any
-): Promise<{seq_no: number; primary_term: number} | null> {
-  try {
-    const client = await getOpenSearchClient();
-
-    log.info(`Making request to ${policyPath}`);
-
-    const response = await client.http.get({
-      path: policyPath,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.statusCode === 200) {
-      const data = response.body;
-      if (
-        data &&
-        data._seq_no !== undefined &&
-        data._primary_term !== undefined
-      ) {
-        return {
-          seq_no: data._seq_no,
-          primary_term: data._primary_term,
-        };
-      } else {
-        log.error().err(data).msg('Required fields not found in the response');
-        return null;
-      }
-    } else {
-      log.error().err(response).msg('Failed to fetch policy');
-      throw new Error(`Request failed with status: ${response.statusCode}`);
-    }
-  } catch (error: any) {
-    log.error().err(error.message).msg('Error fetching policy');
-    throw new Error(`Error fetching policy: ${error.message}`);
-  }
-}
-async function post(
-  policyPath: string,
-  bodyObject: any,
-  seqNo: string,
-  primaryTerm: string,
-  log: any
-): Promise<any> {
-  const queryParams: Record<string, any> = {};
-  if (seqNo !== null && primaryTerm !== null) {
-    queryParams.if_seq_no = seqNo;
-    queryParams.if_primary_term = primaryTerm;
-  }
-
-  try {
-    const client = await getOpenSearchClient();
-
-    const response = await client.http.put({
-      path: policyPath,
-      body: bodyObject,
-      querystring: queryParams,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.statusCode === 200) {
-      log
-        .info()
-        .str('response', response.body)
-        .msg('ISM policy updated successfully');
-      return response.body;
-    } else {
-      log.error().err(response).msg('Failed to update ISM policy');
-      throw new Error(
-        `Failed to update ISM policy with status: ${response.statusCode}`
-      );
-    }
-  } catch (e: any) {
-    log.error().err(e).msg('Error processing request');
-    throw new Error(`Error processing request: ${e.message}`);
-  }
+  log.info().str('policyId', policyId).msg('Policy updated');
 }
--- handlers/src/open-search-helper.ts
+++ handlers/src/open-search-helper.ts
@@ -1,28 +1,182 @@
-import * as opensearch from '@opensearch-project/opensearch';
 import {AwsSigv4Signer} from '@opensearch-project/opensearch/aws';
 import {
   AssumeRoleCommand,
   AssumeRoleCommandOutput,
   STSClient,
 } from '@aws-sdk/client-sts';
-
-export const OPEN_SEARCH_MASTER_ROLE_ARN =
-  process.env.OPEN_SEARCH_MASTER_ROLE_ARN;
-if (!OPEN_SEARCH_MASTER_ROLE_ARN) {
-  throw new Error('OPEN_SEARCH_MASTER_ROLE_ARN is required');
-}
-
-export const OPEN_SEARCH_ENDPOINT = process.env.OPEN_SEARCH_ENDPOINT;
-if (!OPEN_SEARCH_ENDPOINT) {
-  throw new Error('OPEN_SEARCH_ENDPOINT is required');
-}
+import {Client} from '@opensearch-project/opensearch';
+import {DeepPartial} from './ts-helper';
 
 const stsClient = new STSClient({});
 
-export async function getOpenSearchClient(): Promise<opensearch.Client> {
+interface EnvConfig {
+  readonly masterRoleArn: string;
+  readonly endpoint: string;
+  readonly region: string;
+}
+
+let config: EnvConfig | null = null;
+
+function getConfig(): EnvConfig {
+  if (config !== null) {
+    return config;
+  }
+  const masterRoleArn = process.env.OPEN_SEARCH_MASTER_ROLE_ARN;
+  if (!masterRoleArn) {
+    throw new Error('OPEN_SEARCH_MASTER_ROLE_ARN is required');
+  }
+  const endpoint = process.env.OPEN_SEARCH_ENDPOINT;
+  if (!endpoint) {
+    throw new Error('OPEN_SEARCH_ENDPOINT is required');
+  }
+  const region = process.env.AWS_REGION;
+  if (!region) {
+    throw new Error('AWS_REGION is required');
+  }
+  config = {
+    masterRoleArn,
+    endpoint,
+    region,
+  };
+  return config;
+}
+
+export function getOpenSearchEndpoint(): string {
+  return getConfig().endpoint;
+}
+
+export interface IsmPolicy {
+  policy_id: string;
+  description: string;
+  last_updated_time: number;
+  schema_version: number;
+  default_state: string;
+  states: Array<{
+    name: string;
+    actions: Array<{
+      action_type: string;
+      retry: {
+        count: number;
+        backoff: string;
+        delay: string;
+      };
+      throttle: {
+        value: number;
+        unit: string;
+      };
+      delete: {};
+    }>;
+    transitions: null | Array<{
+      state_name: string;
+      conditions: {
+        min_index_age: string;
+      };
+    }>;
+  }>;
+  ism_template: Array<{
+    index_patterns: string[];
+    priority: number;
+  }>;
+}
+
+export type PartialIsmPolicy = DeepPartial<IsmPolicy>;
+
+export interface IsmPolicyVersion {
+  _id: string;
+  _version: number;
+  _seq_no: number;
+  _primary_term: number;
+  policy: IsmPolicy;
+}
+
+export interface RoleMapping {
+  hosts: string[];
+  users: string[];
+  reserved: boolean;
+  hidden: boolean;
+  backend_roles: string[];
+  and_backend_roles: string[];
+}
+
+export type RolesMapping = Record<string, RoleMapping>;
+
+export interface RoleMappingUpdate {
+  hosts: string[];
+  users: string[];
+  backend_roles: string[];
+}
+
+export class OpenSearchClient extends Client {
+  ism = {
+    getPolicy: async (policyId: string): Promise<IsmPolicyVersion | null> => {
+      const path = `/_plugins/_ism/policies/${policyId}`;
+      const {body} = await this.transport.request({
+        method: 'GET',
+        path,
+      });
+      if (!body) {
+        return null;
+      }
+      return body as IsmPolicyVersion;
+    },
+
+    updatePolicy: async (
+      policyId: string,
+      policy: PartialIsmPolicy,
+      seqNo?: number,
+      primaryTerm?: number
+    ): Promise<void> => {
+      const path = `/_plugins/_ism/policies/${policyId}`;
+      const querystring: Record<string, string | number> = {};
+      if (seqNo && primaryTerm) {
+        querystring.if_seq_no = seqNo;
+        querystring.if_primary_term = primaryTerm;
+      }
+      await this.transport.request({
+        method: 'PUT',
+        path,
+        querystring,
+        body: {policy},
+      });
+    },
+  };
+
+  sec = {
+    getRoleMapping: async (name: string): Promise<RoleMapping | null> => {
+      const path = `/_plugins/_security/api/rolesmapping/${name}`;
+      const {body} = await this.transport.request({
+        method: 'GET',
+        path,
+      });
+      if (body[name] === undefined) {
+        return null;
+      }
+      return body[name];
+    },
+
+    updateRoleMapping: async (
+      name: string,
+      mapping: RoleMappingUpdate
+    ): Promise<void> => {
+      const path = `/_plugins/_security/api/rolesmapping/${name}`;
+      await this.transport.request({
+        method: 'PUT',
+        path,
+        body: {
+          // Done to strip out extra properties
+          hosts: mapping.hosts,
+          users: mapping.users,
+          backend_roles: mapping.backend_roles,
+        },
+      });
+    },
+  };
+}
+
+export async function getOpenSearchClient(): Promise<OpenSearchClient> {
   const response: AssumeRoleCommandOutput = await stsClient.send(
     new AssumeRoleCommand({
-      RoleArn: OPEN_SEARCH_MASTER_ROLE_ARN,
+      RoleArn: getConfig().masterRoleArn,
       RoleSessionName: 'overwatch-config-handler',
     })
   );
@@ -34,13 +188,13 @@ export async function getOpenSearchClient(): Promise<opensearch.Client> {
     secretAccessKey: response.Credentials.SecretAccessKey!,
     sessionToken: response.Credentials.SessionToken!,
   };
-  return new opensearch.Client({
+  return new OpenSearchClient({
     ...AwsSigv4Signer({
-      region: process.env.AWS_REGION!,
+      region: getConfig().region,
       getCredentials: async () => {
         return credentials;
       },
     }),
-    node: OPEN_SEARCH_ENDPOINT,
+    node: getConfig().endpoint,
   });
 }
