ed6a490b7034485b381297f5e7336242d322b564
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-24 13:10:56 -0600

    Restructuring execution to support multiple stacks. Adding AccountAlarms construct

--- cdk/bin/overwatch.ts
+++ cdk/bin/overwatch.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import {ExtendedApp} from 'truemark-cdk-lib/aws-cdk';
 import {OverwatchStack} from '../lib/overwatch-stack';
+import {OverwatchSupportStack} from '../lib/overwatch-support-stack';
 
 const app = new ExtendedApp({
   standardTags: {
@@ -12,64 +13,14 @@ const app = new ExtendedApp({
   },
 });
 
-const idpEntityId = app.node.tryGetContext('idpEntityId');
-if (!idpEntityId) {
-  throw new Error('Missing idpEntityId in context');
+const stack = app.node.tryGetContext('stack');
+if (!stack) {
+  throw new Error('stack is required in context');
 }
-const idpMetadataContent = app.node.tryGetContext('idpMetadataContent');
-if (!idpMetadataContent) {
-  throw new Error('Missing idpMetadataContent in context');
-}
-const domainName = app.node.tryGetContext('domainName');
-if (!domainName) {
-  throw new Error('Missing domainName in context');
-}
-const zoneName = app.node.tryGetContext('zoneName');
-if (!zoneName) {
-  throw new Error('Missing zoneName in context');
-}
-const hostedZoneId = app.node.tryGetContext('zoneId');
-if (!hostedZoneId) {
-  throw new Error('Missing zoneId in context');
-}
-const masterBackendRole = app.node.tryGetContext('masterBackendRole');
-if (!masterBackendRole) {
-  throw new Error('Missing masterBackendRole in context');
-}
-let accountIds = app.node.tryGetContext('accountIds');
-if (!accountIds) {
-  throw new Error('Missing accountIds in context');
-}
-accountIds = accountIds.split(',');
-let adminGroups = app.node.tryGetContext('adminGroups');
-if (adminGroups) {
-  adminGroups = adminGroups.split(',');
-}
-let editorGroups = app.node.tryGetContext('editorGroups');
-if (editorGroups) {
-  editorGroups = editorGroups.split(',');
-}
-let organizationalUnits = app.node.tryGetContext('organizationalUnits');
-if (!organizationalUnits) {
-  throw new Error('Missing organizationalUnits in context');
+if (stack === 'overwatch') {
+  OverwatchStack.fromContext(app, 'Overwatch');
+} else if (stack === 'support') {
+  OverwatchSupportStack.fromContext(app, 'OverwatchSupport');
 } else {
-  organizationalUnits = organizationalUnits.split(',');
+  throw new Error(`Unknown stack: ${stack}`);
 }
-
-new OverwatchStack(app, 'Overwatch', {
-  volumeSize: 4096,
-  idpEntityId,
-  idpMetadataContent,
-  masterBackendRole,
-  hostedDomainName: {
-    domainName,
-    zone: {
-      hostedZoneId,
-      zoneName,
-    },
-  },
-  accountIds,
-  adminGroups,
-  editorGroups,
-  organizationalUnits,
-});
--- cdk/lib/overwatch-stack.ts
+++ cdk/lib/overwatch-stack.ts
@@ -1,25 +1,87 @@
 import {ExtendedStack, ExtendedStackProps} from 'truemark-cdk-lib/aws-cdk';
 import {Construct} from 'constructs';
-import {Overwatch} from './overwatch-construct';
-import {HostedDomainNameProps} from './standard-domain';
+import {Overwatch, OverwatchProps} from './overwatch-construct';
+import {App} from 'aws-cdk-lib';
 
-export interface OverwatchStackProps extends ExtendedStackProps {
-  readonly volumeSize?: number;
-  readonly idpEntityId: string;
-  readonly idpMetadataContent: string;
-  readonly masterBackendRole: string;
-  readonly hostedDomainName?: HostedDomainNameProps;
-  readonly accountIds: string[];
-  readonly organizationalUnits: string[];
-  readonly adminGroups?: string[];
-  readonly editorGroups?: string[];
-}
+export interface OverwatchStackProps
+  extends ExtendedStackProps,
+    OverwatchProps {}
 
 export class OverwatchStack extends ExtendedStack {
   constructor(scope: Construct, id: string, props: OverwatchStackProps) {
     super(scope, id, props);
-    new Overwatch(this, 'Default', {
-      ...props,
-    });
+    new Overwatch(this, 'Default', props);
+  }
+
+  static propsFromContext(app: App): OverwatchStackProps {
+    let volumeSize = app.node.tryGetContext('volumeSize');
+    if (!volumeSize) {
+      throw new Error('volumeSize is required in context');
+    }
+    volumeSize = parseInt(volumeSize, 10);
+    const idpEntityId = app.node.tryGetContext('idpEntityId');
+    if (!idpEntityId) {
+      throw new Error('idpEntityId is required in context');
+    }
+    const idpMetadataContent = app.node.tryGetContext('idpMetadataContent');
+    if (!idpMetadataContent) {
+      throw new Error('idpMetadataContent is required in context');
+    }
+    const domainName = app.node.tryGetContext('domainName');
+    if (!domainName) {
+      throw new Error('Missing domainName in context');
+    }
+    const zoneName = app.node.tryGetContext('zoneName');
+    if (!zoneName) {
+      throw new Error('Missing zoneName in context');
+    }
+    const hostedZoneId = app.node.tryGetContext('zoneId');
+    if (!hostedZoneId) {
+      throw new Error('Missing zoneId in context');
+    }
+    const masterBackendRole = app.node.tryGetContext('masterBackendRole');
+    if (!masterBackendRole) {
+      throw new Error('openSearchMasterBackendRole is required in context');
+    }
+    let accountIds = app.node.tryGetContext('accountIds');
+    if (!accountIds) {
+      throw new Error('Missing accountIds in context');
+    }
+    accountIds = accountIds.split(',');
+    let adminGroups = app.node.tryGetContext('adminGroups');
+    if (adminGroups) {
+      adminGroups = adminGroups.split(',');
+    }
+    let editorGroups = app.node.tryGetContext('editorGroups');
+    if (editorGroups) {
+      editorGroups = editorGroups.split(',');
+    }
+    let organizationalUnits = app.node.tryGetContext('organizationalUnits');
+    if (!organizationalUnits) {
+      throw new Error('Missing organizationalUnits in context');
+    } else {
+      organizationalUnits = organizationalUnits.split(',');
+    }
+    return {
+      volumeSize,
+      idpEntityId,
+      idpMetadataContent,
+      masterBackendRole,
+      hostedDomainName: {
+        domainName,
+        zone: {
+          hostedZoneId,
+          zoneName,
+        },
+      },
+      accountIds,
+      adminGroups,
+      editorGroups,
+      organizationalUnits,
+    };
+  }
+
+  static fromContext(app: App, id: string): OverwatchStack {
+    return new OverwatchStack(app, id, OverwatchStack.propsFromContext(app));
   }
 }
