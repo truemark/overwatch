ba3c988b7f334a6a2c02b785dcc588526efba81e
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-24 10:56:46 -0600

    Stubbing in construct and stack for support

---
+++ cdk/lib/overwatch-support-construct.ts
@@ -0,0 +1,12 @@
+// TODO
+import {Construct} from 'constructs';
+
+export interface OverwatchSupportProps {
+  readonly somethingsomething: string;
+}
+
+export class OverwatchSupport extends Construct {
+  constructor(scope: Construct, id: string, props: OverwatchSupportProps) {
+    super(scope, id);
+  }
+}
