7053f2189a481c7d817b217c9d9c298b61ae7558
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-06-25 14:28:42 -0600

    Refactoring

--- cdk/lib/overwatch-support-construct.ts
+++ cdk/lib/overwatch-support-construct.ts
@@ -1,19 +1,16 @@
 import {Construct} from 'constructs';
 import {StringParameter, CfnDocument} from 'aws-cdk-lib/aws-ssm';
-import {
-  IVpc,
-  SecurityGroup,
-  Port,
-  Peer,
-  CfnInstance,
-} from 'aws-cdk-lib/aws-ec2';
+import {IVpc, SecurityGroup, Port, Peer} from 'aws-cdk-lib/aws-ec2';
 import {PrometheusScraper} from './prometheus-scraper';
 import {Cluster} from 'aws-cdk-lib/aws-ecs';
-import {CfnRuleGroupsNamespace, CfnWorkspace} from 'aws-cdk-lib/aws-aps';
+import {CfnWorkspace} from 'aws-cdk-lib/aws-aps';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export interface OverwatchSupportConstructProps {
   readonly vpc: IVpc;
 }
+
 export class OverwatchSupportConstruct extends Construct {
   constructor(
     scope: Construct,
@@ -22,67 +19,20 @@ export class OverwatchSupportConstruct extends Construct {
   ) {
     super(scope, id);
 
-    const prometheusWorkspace = new CfnWorkspace(this, 'OverwatchWorkspace', {
-      alias: 'OverwatchMetrics', // Optional alias for the workspace
+    const prometheusWorkspace = new CfnWorkspace(this, 'Workspace', {
+      alias: 'Overwatch',
     });
-    // Create a Rule Groups Namespace
-    // Create a Rule Groups Namespace
-    const ruleGroupsNamespace = new CfnRuleGroupsNamespace(
-      this,
-      'MyRuleGroupsNamespace',
-      {
-        workspace: prometheusWorkspace.ref,
-        name: 'default', // Name of the rule groups namespace
-        data: JSON.stringify({
-          groups: [
-            {
-              name: 'example.rules',
-              rules: [
-                {
-                  alert: 'HighCpuUsage',
-                  expr: 'avg by (instance) (rate(node_cpu_seconds_total{mode!="idle"}[5m])) > 0.95',
-                  for: '5m',
-                  labels: {
-                    severity: 'critical',
-                  },
-                  annotations: {
-                    summary:
-                      'Instance {{ $labels.instance }} has high CPU usage',
-                    description:
-                      'CPU usage is above 95% for more than 5 minutes.',
-                  },
-                },
-              ],
-            },
-          ],
-        }),
-      }
+
+    // eslint-disable-next-line n/no-unsupported-features/node-builtins
+    const nodeExporterServiceConfig = fs.readFileSync(
+      path.join(__dirname, '..', '..', 'support', 'node_exporter.service'),
+      'utf-8'
     );
-    // Create Security group for Prometheus data to be scraped
-    const vpc = props.vpc;
-    // Create a security group within the VPC
-
-    const nodeExporterServiceConfig = `
-    [Unit]
-    Description=Node Exporter
-    After=network.target
-
-    [Service]
-    User=prometheus
-    ExecStart=/etc/prometheus/node_exporter/node_exporter
-
-    [Install]
-    WantedBy=default.target
-    `;
-    const nodeExporterServiceConfigParam = new StringParameter(
-      this,
-      'NodeExporterServiceConfigParam',
-      {
-        parameterName: '/prometheus-config/NodeExporter-ServiceConfig',
-        stringValue: nodeExporterServiceConfig,
-        description: 'The Node Exporter service configuration',
-      }
-    );
+    new StringParameter(this, 'NodeExporterServiceConfigParam', {
+      parameterName: '/overwatch/prometheus-config/NodeExporter-ServiceConfig',
+      stringValue: nodeExporterServiceConfig,
+      description: 'The Node Exporter service configuration',
+    });
     // Document for installing windows node exporter
     const documentContent = {
       schemaVersion: '2.2',
@@ -107,7 +57,8 @@ export class OverwatchSupportConstruct extends Construct {
           type: 'String',
         },
         NodeExporterServiceConfig: {
-          default: '{{ ssm:/prometheus-config/NodeExporter-ServiceConfig }}',
+          default:
+            '{{ ssm:/overwatch/prometheus-config/NodeExporter-ServiceConfig }}',
           description: 'The Node Exporter service configuration',
           type: 'String',
         },
@@ -120,20 +71,18 @@ export class OverwatchSupportConstruct extends Construct {
           action: 'aws:runPowerShellScript',
           name: 'InstallNodeExporterWindows',
           inputs: {
-            runCommand: [
-              'Invoke-WebRequest -Uri {{ WindowsExporterPackageUrl }} -OutFile C:\\windows\\temp\\windows_exporter.msi',
-              "Start-Process msiexec.exe -ArgumentList '/i C:\\windows\\temp\\windows_exporter.msi /quiet' -NoNewWindow -Wait",
-              'Remove-Item C:\\windows\\temp\\windows_exporter.msi',
-              "# Stop the service if it's running",
-              'Stop-Service -Name windows_exporter -ErrorAction SilentlyContinue',
-              '# Delete the existing service',
-              'sc.exe delete windows_exporter',
-              '$ErrorActionPreference = "Stop"',
-              '$commandLine =\'"C:/Program Files/windows_exporter/windows_exporter.exe" --web.listen-address=:9100\'',
-              "New-Service -Name 'windows_exporter' -BinaryPathName $commandLine -DisplayName 'Windows Exporter' -StartupType Automatic",
-              'Start-Service -Name "windows_exporter"',
-              'New-NetFirewallRule -DisplayName "Allow Node Exporter" -Direction Inbound -Protocol TCP -LocalPort 9100 -Action Allow',
-            ],
+            runCommand: fs
+              .readFileSync(
+                path.join(
+                  __dirname,
+                  '..',
+                  '..',
+                  'support',
+                  'install_node_exporter.ps1'
+                ),
+                'utf-8'
+              )
+              .split('\n'),
           },
         },
         {
@@ -143,36 +92,18 @@ export class OverwatchSupportConstruct extends Construct {
           action: 'aws:runShellScript',
           name: 'InstallNodeExporterLinux',
           inputs: {
-            runCommand: [
-              // Determine architecture and set package URLs accordingly
-              'ARCH=$(uname -m)',
-              'if [ "$ARCH" == "x86_64" ]; then',
-              "  NODE_EXPORTER_PACKAGE_URL='{{ NodeExporterPackageUrl }}'",
-              'elif [ "$ARCH" == "aarch64" ]; then',
-              "  NODE_EXPORTER_PACKAGE_URL='{{ NodeExporterPackageUrlArm }}'",
-              'else',
-              "  echo 'Unsupported architecture: $ARCH'",
-              '  exit 1',
-              'fi',
-              'sudo useradd --no-create-home --shell /bin/false prometheus | >/dev/null 2>&1',
-              'sudo mkdir -p /etc/prometheus',
-              'sudo mkdir -p /var/lib/prometheus',
-              'sudo chown prometheus:prometheus /etc/prometheus',
-              'sudo chown prometheus:prometheus /var/lib/prometheus',
-              "echo 'Downloading and configuring Node Exporter'",
-              'sudo systemctl stop node_exporter >/dev/null 2>&1',
-              'wget $NODE_EXPORTER_PACKAGE_URL >/dev/null 2>&1',
-              'tar xfz node_exporter-*.tar.gz',
-              'mv node_exporter-*64 node_exporter',
-              'sudo rm -rf /etc/prometheus/node_exporter',
-              'sudo mv node_exporter/ /etc/prometheus/node_exporter/',
-              'rm node_exporter-*.tar.gz',
-              'sudo chown -R prometheus:prometheus /etc/prometheus/node_exporter',
-              'sudo echo "{{ NodeExporterServiceConfig }}" > /etc/systemd/system/node_exporter.service',
-              'sudo systemctl daemon-reload',
-              'sudo systemctl enable node_exporter >/dev/null 2>&1',
-              'sudo systemctl start node_exporter',
-            ],
+            runCommand: fs
+              .readFileSync(
+                path.join(
+                  __dirname,
+                  '..',
+                  '..',
+                  'support',
+                  'install_node_exporter.sh'
+                ),
+                'utf-8'
+              )
+              .split('\n'),
           },
         },
       ],
@@ -183,17 +114,19 @@ export class OverwatchSupportConstruct extends Construct {
       name: 'InstallNodeExporter',
       updateMethod: 'NewVersion',
     });
+
     // Create and overwatch ECS cluster
     const cluster = new Cluster(this, 'Overwatch', {
-      vpc: vpc,
+      vpc: props.vpc,
     });
     // Create Prometheus scraper
     const ecsPrometheus = new PrometheusScraper(this, 'PrometheusScraper', {
       cluster: cluster,
       workspace: prometheusWorkspace,
     });
+
     const securityGroup = new SecurityGroup(this, 'PrometheusSecurityGroup', {
-      vpc,
+      vpc: props.vpc,
       securityGroupName: 'prometheus-sg',
       description: 'Allow Prometheus scraping on port 9100 within the VPC',
       allowAllOutbound: true,
@@ -206,8 +139,5 @@ export class OverwatchSupportConstruct extends Construct {
       Port.tcp(9100),
       'Allow Prometheus scraping'
     );
-    //   new PrometheusScraper();
-    //
-    //   // TODO Add role assignment for Grafana
   }
 }
