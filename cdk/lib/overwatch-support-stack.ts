c2a80f6eb4ca7bea7daaad212143918cc6a8efce
Trent Cameron <tcameron@truemark.io>
2024-06-26 12:37:40 -0600

    added role

--- cdk/lib/overwatch-support-stack.ts
+++ cdk/lib/overwatch-support-stack.ts
@@ -4,12 +4,20 @@ import {OverwatchSupportConstruct} from './overwatch-support-construct';
 import {Vpc} from 'aws-cdk-lib/aws-ec2';
 import {App} from 'aws-cdk-lib';
 import {AccountAlarms} from './account-alarms';
+import {
+  AccountPrincipal,
+  CompositePrincipal,
+  Effect,
+  PolicyStatement,
+  Role,
+} from 'aws-cdk-lib/aws-iam';
 
 export interface OverwatchSupportStackProps extends ExtendedStackProps {
   readonly primaryRegion?: boolean;
   readonly vpcId: string;
   readonly availabilityZones: string[];
   readonly privateSubnetIds: string[];
+  readonly trustedAccountList: string[];
 }
 
 export class OverwatchSupportStack extends ExtendedStack {
@@ -17,8 +25,172 @@ export class OverwatchSupportStack extends ExtendedStack {
     super(scope, id, props);
 
     if (props.primaryRegion ?? true) {
-      // TODO Create observability role
       new AccountAlarms(this, 'Alarms');
+
+      const trustRelationships = new CompositePrincipal(
+        ...props.trustedAccountList.map(
+          accountId => new AccountPrincipal(accountId)
+        )
+      );
+      const role = new Role(this, 'OverwatchObservability', {
+        assumedBy: trustRelationships,
+        description:
+          'This role is used to allow grafana to access logs and metrics.',
+      });
+      const policies = [
+        {
+          sid: 'AllowReadingMetricsFromCloudWatch',
+          actions: [
+            'cloudwatch:ListMetrics',
+            'cloudwatch:GetMetricStatistics',
+            'cloudwatch:GetMetricData',
+            'cloudwatch:DescribeAlarmsForMetric',
+            'cloudwatch:DescribeAlarms',
+            'cloudwatch:DescribeAlarmHistory',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowReadingLogsFromCloudWatch',
+          actions: [
+            'logs:StopQuery',
+            'logs:StartQuery',
+            'logs:GetQueryResults',
+            'logs:GetLogGroupFields',
+            'logs:GetLogEvents',
+            'logs:DescribeLogGroups',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowReadingTagsInstancesRegionsFromEC2',
+          actions: [
+            'ec2:DescribeTags',
+            'ec2:DescribeRegions',
+            'ec2:DescribeInstances',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowReadingResourcesForTags',
+          actions: ['tag:GetResources'],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowESGet',
+          actions: [
+            'es:ListDomainNames',
+            'es:ESHttpGet',
+            'es:DescribeElasticsearchDomains',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowESPost',
+          actions: ['es:ESHttpPost'],
+          resources: [
+            'arn:aws:es:*:*:domain/*/_opendistro/_ppl',
+            'arn:aws:es:*:*:domain/*/_msearch*',
+          ],
+        },
+        {
+          sid: 'AllowAMPReadOnly',
+          actions: [
+            'aps:QueryMetrics',
+            'aps:ListWorkspaces',
+            'aps:GetSeries',
+            'aps:GetMetricMetadata',
+            'aps:GetLabels',
+            'aps:DescribeWorkspace',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowAthenaReadOnly',
+          actions: [
+            'athena:ListWorkGroups',
+            'athena:ListTableMetadata',
+            'athena:ListDatabases',
+            'athena:ListDataCatalogs',
+            'athena:GetTableMetadata',
+            'athena:GetDatabase',
+            'athena:GetDataCatalog',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowAthenaOperations',
+          actions: [
+            'athena:StopQueryExecution',
+            'athena:StartQueryExecution',
+            'athena:GetWorkGroup',
+            'athena:GetQueryResults',
+            'athena:GetQueryExecution',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowGlueReadOnly',
+          actions: [
+            'glue:GetTables',
+            'glue:GetTable',
+            'glue:GetPartitions',
+            'glue:GetPartition',
+            'glue:GetDatabases',
+            'glue:GetDatabase',
+            'glue:BatchGetPartition',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowReadingTracesFromXRay',
+          actions: [
+            'xray:GetTraceSummaries',
+            'xray:GetTraceGraph',
+            'xray:GetTimeSeriesServiceStatistics',
+            'xray:GetServiceGraph',
+            'xray:GetInsightSummaries',
+            'xray:GetInsight',
+            'xray:GetGroups',
+            'xray:BatchGetTraces',
+            'ec2:DescribeRegions',
+          ],
+          resources: ['*'],
+        },
+        {
+          sid: 'AllowAOSSOperations',
+          actions: [
+            'aoss:BatchGetCollection',
+            'aoss:ListLifecyclePolicies',
+            'aoss:BatchGetLifecyclePolicy',
+            'aoss:ListSecurityConfigs',
+            'aoss:GetAccessPolicy',
+            'aoss:ListAccessPolicies',
+            'aoss:ListSecurityPolicies',
+            'aoss:BatchGetVpcEndpoint',
+            'aoss:GetPoliciesStats',
+            'aoss:ListVpcEndpoints',
+            'aoss:GetAccountSettings',
+            'aoss:GetSecurityConfig',
+            'aoss:BatchGetEffectiveLifecyclePolicy',
+            'aoss:ListCollections',
+            'aoss:GetSecurityPolicy',
+            'aoss:ListTagsForResource',
+            'aoss:APIAccessAll',
+          ],
+          resources: ['*'],
+        },
+      ];
+      policies.forEach(policy => {
+        role.addToPolicy(
+          new PolicyStatement({
+            sid: policy.sid,
+            effect: Effect.ALLOW,
+            actions: policy.actions,
+            resources: policy.resources,
+          })
+        );
+      });
     }
 
     const vpc = Vpc.fromVpcAttributes(this, 'Vpc', {
@@ -57,6 +229,7 @@ export class OverwatchSupportStack extends ExtendedStack {
       vpcId,
       availabilityZones,
       privateSubnetIds,
+      trustedAccountList,
     };
   }
 
