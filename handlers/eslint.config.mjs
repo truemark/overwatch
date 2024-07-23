176f9ad9dee4623270dd05c01e0207dabe34cca2
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-07-23 17:22:42 -0600

    Fixing eslint

--- handlers/eslint.config.mjs
+++ handlers/eslint.config.mjs
@@ -3,9 +3,13 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-const config = tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
-config[0].ignores = ['src/**/*.mjs'];
+const config = [
+  {
+    ignores: ['src/**/*.mjs', 'src/**/*.d.*'],
+  },
+  ...tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+  ),
+];
 export default config;
