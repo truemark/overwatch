99720d082fa1b0a8c77ef782d182c705ee1cd726
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-07-24 13:46:10 -0600

    Removing metrics and how stacks are run

--- cdk/bin/overwatch.ts
+++ cdk/bin/overwatch.ts
@@ -3,7 +3,6 @@ import 'source-map-support/register';
 import {ExtendedApp} from 'truemark-cdk-lib/aws-cdk';
 import {OverwatchStack} from '../lib/overwatch';
 import {OverwatchSupportStack} from '../lib/support';
-import {OverwatchMetricsStack} from '../lib/metrics';
 
 const app = new ExtendedApp({
   standardTags: {
@@ -14,16 +13,5 @@ const app = new ExtendedApp({
   },
 });
 
-const stack = app.node.tryGetContext('stack');
-if (!stack) {
-  throw new Error('stack is required in context');
-}
-if (stack === 'overwatch') {
-  OverwatchStack.fromContext(app, 'Overwatch');
-} else if (stack === 'support') {
-  OverwatchSupportStack.fromContext(app, 'OverwatchSupport');
-} else if (stack === 'metrics') {
-  OverwatchMetricsStack.fromContext(app, 'OverwatchMetrics');
-} else {
-  throw new Error(`Unknown stack: ${stack}`);
-}
+OverwatchStack.fromContext(app, 'Overwatch');
+OverwatchSupportStack.fromContext(app, 'OverwatchSupport');
